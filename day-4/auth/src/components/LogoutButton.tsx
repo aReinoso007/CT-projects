bca200dcc51c0e3c988b323232fa619b753ea9b5
Alex Reinoso <ralexjessiel@gmail.com>
2024-02-18 20:37:06 -0500

    logout button

---
+++ day-4/auth/src/components/LogoutButton.tsx
@@ -0,0 +1,25 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "react-bootstrap"
+
+const LogoutButton: React.FC = () =>{
+    const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+
+    return(
+        <Button
+          onClick={handleLogout}
+        >
+         Log Out
+        </Button>
+    )
+}
+
+export default LogoutButton;
--- day-4/auth/src/components/SignupButton.tsx
+++ day-4/auth/src/components/SignupButton.tsx
@@ -1,8 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "react-bootstrap";
 
 const SignupButton: React.FC = () => {
-
-    const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   const handleSignUp = async () => {
     await loginWithRedirect({
@@ -15,10 +15,7 @@ const SignupButton: React.FC = () => {
     });
   };
 
+  return <Button onClick={handleSignUp}>Sign up</Button>;
+};
 
-    return (
-        <button>Sign Up</button>
-    );
-}
-
-export default SignupButton;   
+export default SignupButton;

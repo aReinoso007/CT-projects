3617edab830f0ec654a97d4d5bc45228ba49f4e8
Alex Reinoso <ralexjessiel@gmail.com>
2024-02-24 15:55:29 -0500

    validation checking

---
+++ module-12/lesson-5/flask-ecommerce-ui/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+// src/components/HomePage.jsx
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Our E-Commerce App</h1>
+      <p>This is the place to find all your needs at one click.</p>
+    </div>
+  );
+}
+
+export default HomePage;
